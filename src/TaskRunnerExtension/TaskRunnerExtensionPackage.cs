2cf2fd785e94fa53b5e6871765786cebdaa013fd
Mads Kristensen <post@madskristensen.net>
2016-09-06 13:57:18 -0700

    Added ProvideMenuResource attribute

--- src/TaskRunnerExtension/TaskRunnerExtensionPackage.cs
+++ src/TaskRunnerExtension/TaskRunnerExtensionPackage.cs
@@ -7,6 +7,7 @@ namespace TaskRunnerExtension
     [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
     [InstalledProductRegistration("#110", "#112", Vsix.Version, IconResourceID = 400)]
     [Guid(PackageGuids.guidVSPackageString)]
+    [ProvideMenuResource("Menus.ctmenu", 1)]
     public sealed class TaskRunnerExtensionPackage : Package
     {
         protected override void Initialize()
